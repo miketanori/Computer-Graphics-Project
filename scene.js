8baed71922510e9d55faa2e624813e8a921357e5
Mike Tanori <miguel.tanori@hotmail.com>
2022-10-06 14:08:47 -0500

    Water

--- scene.js
+++ scene.js
@@ -1,24 +1,122 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+import * as THREE from 'three';
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { Water } from 'three/examples/jsm/objects/Water.js';
+import { Sky } from 'three/examples/jsm/objects/Sky.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+let camera, scene, renderer;
+let controls, water, sun;
 
-camera.position.z = 5;
+const loader = new GLTFLoader();
+
+init();
+animate();
+
+async function init() {
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  document.body.appendChild( renderer.domElement );
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 1, 20000 );
+  camera.position.set( 30, 30, 100 );
+
+  sun = new THREE.Vector3();
+
+  // Water
+
+  const waterGeometry = new THREE.PlaneGeometry( 10000, 10000 );
+
+  water = new Water(
+    waterGeometry,
+    {
+      textureWidth: 512,
+      textureHeight: 512,
+      waterNormals: new THREE.TextureLoader().load( 'assets/waternormals.jpg', function ( texture ) {
+
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+
+      } ),
+      sunDirection: new THREE.Vector3(),
+      sunColor: 0xffffff,
+      waterColor: 0x001e0f,
+      distortionScale: 3.7,
+      fog: scene.fog !== undefined
+    }
+  );
+
+  water.rotation.x = - Math.PI / 2;
+
+  scene.add( water );
+
+  // Skybox
+
+  const sky = new Sky();
+  sky.scale.setScalar( 10000 );
+  scene.add( sky );
+
+  const skyUniforms = sky.material.uniforms;
+
+  skyUniforms[ 'turbidity' ].value = 10;
+  skyUniforms[ 'rayleigh' ].value = 2;
+  skyUniforms[ 'mieCoefficient' ].value = 0.005;
+  skyUniforms[ 'mieDirectionalG' ].value = 0.8;
+
+  const parameters = {
+    elevation: 2,
+    azimuth: 180
+  };
+
+  const pmremGenerator = new THREE.PMREMGenerator( renderer );
+
+  function updateSun() {
+
+    const phi = THREE.MathUtils.degToRad( 90 - parameters.elevation );
+    const theta = THREE.MathUtils.degToRad( parameters.azimuth );
+
+    sun.setFromSphericalCoords( 1, phi, theta );
+
+    sky.material.uniforms[ 'sunPosition' ].value.copy( sun );
+    water.material.uniforms[ 'sunDirection' ].value.copy( sun ).normalize();
+
+    scene.environment = pmremGenerator.fromScene( sky ).texture;
+
+  }
+
+  updateSun();
+
+  controls = new OrbitControls( camera, renderer.domElement );
+  controls.maxPolarAngle = Math.PI * 0.495;
+  controls.target.set( 0, 10, 0 );
+  controls.minDistance = 40.0;
+  controls.maxDistance = 200.0;
+  controls.update();
+
+  const waterUniforms = water.material.uniforms;
+
+  window.addEventListener( 'resize', onWindowResize );
+
+  function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
 
 function animate() {
-	requestAnimationFrame( animate );
+  requestAnimationFrame( animate );
+  render();
+}
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+function render() {
+  water.material.uniforms[ 'time' ].value += 1.0 / 60.0;
 
-	renderer.render( scene, camera );
-};
+  renderer.render( scene, camera );
 
-animate();
+}
