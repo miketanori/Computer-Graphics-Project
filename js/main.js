b43fe97adaef470686f80dacaa7356c07ff088e2
Mike Tanori <miguel.tanori@hotmail.com>
2022-11-12 22:08:19 -0600

    Boat y box

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
 let camera, scene, renderer;
 let controls, water, sky, sun;
 
-  init();
-  animate();
-
-
+let shipTurning = false, shipDir = 'left';
+const shipGroup = new THREE.Group();
 function init() {
   // CONTAINER
   container = document.createElement( 'div' );
   document.body.appendChild( container );
 
   // SCENE
-  // Create a new Three.js scene
+  // Create a new Three.j0s scene
   scene = new THREE.Scene();
   
   // CAMERA
   // Create a camera so we can view the scene
   camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 1, 20000 );
   //camera.position.set( 30, 30, 100 );
-  camera.position.set( 0, 250, 1750 );
+  camera.position.set(0, 0,1000 );
 
   // LIGHTS
   // Add initial ambient light to set the tone for the scene.
@@ -89,15 +87,55 @@ function init() {
 
 // Function to let the animations work correctly.
 function animate() {
+  TWEEN.update();
   // Do the animation each frame.
   requestAnimationFrame( animate );
+  // Ship movement and turning
+
+  if (shipTurning) {
+      if (shipDir == 'left') {
+          if (shipGroup.rotation.y < Math.PI) {
+              shipGroup.rotation.y += 0.1;
+              shipTurning = true;
+          } else {
+              shipTurning = false;
+              shipDir = 'right';
+          }
+      } else {
+          if (shipGroup.rotation.y > 0) {
+              shipGroup.rotation.y -= 0.1;
+              shipTurning = true;
+          } else {
+              shipTurning = false;
+              shipDir = 'left';
+          }
+      }
+        //shipGroup.position.set(0,10,600);
+  } else {
+      if (shipDir == 'left') {
+          if (shipGroup.position.x > -500) {
+              shipGroup.position.x -= 1;
+              shipTurning = false;
+          } else {
+              shipTurning = true;
+          }
+      } else {
+          if (shipGroup.position.x < 500) {
+              shipGroup.position.x += 1;
+              shipTurning = false;
+          } else {
+              shipTurning = true;
+          }
+      }
+  }
   // Render the scene.
   render();
 }
 
 // Function to render the scene
 function render() {
-  water.material.uniforms[ 'time' ].value += 0.30 / 60.0;
+
+  water.material.uniforms[ 'time' ].value += 0.30/60 ;
   // Render the scene.
   renderer.render( scene, camera );
 
@@ -118,7 +156,110 @@ function buildObjects() {
   buildTrees();
   buildClouds();
   updateSun();
+  buildBox();
+  buildBoat();
 
+}
+
+// == SHIP ==
+function buildBoat() {
+  // -- Body --
+  // TODO: Add cover (plane) to the ship.
+
+  const texture = new THREE.TextureLoader().load('assets/woodplank.jpg');
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set( 1, 1 );
+  // Add a bump map to be a little more realistic, not required.
+  const material = new THREE.MeshPhongMaterial( { map: texture, bumpMap: texture, bumpScale: 0.8 } );
+
+  geometry = new THREE.CylinderGeometry(1, 0.8, 3, 5, 3, false, 0, Math.PI);
+  let shipBody = new THREE.Mesh(geometry, material);
+  shipBody.rotation.z = 3 * Math.PI / 2;
+  shipBody.castShadow = true;
+  scene.add(shipBody);
+  
+  // -- Tip --
+  geometry = new THREE.TetrahedronGeometry(0.9);
+  let shipTip = new THREE.Mesh(geometry, material);
+  shipTip.position.x = -1.8;
+  shipTip.position.y = -0.35;
+  shipTip.rotation.x = 0.52;
+  shipTip.rotation.y = 0.57;
+  shipTip.rotation.z = 0.77;
+  shipTip.castShadow = true;
+
+  // -- Roof (1/2) --
+  geometry = new THREE.BoxGeometry(1.8, 0.3, 1);
+  let shipRoof1 = new THREE.Mesh(geometry, material);
+  shipRoof1.position.y = 0.15;
+  shipRoof1.castShadow = true;
+  
+  // -- Roof (2/2) --
+  geometry = new THREE.CylinderGeometry(0.6, 0.6, 2.5, 2, 3, false, 0, Math.PI);
+  let shipRoof2 = new THREE.Mesh(geometry, material);
+  shipRoof2.position.y = 0.3;
+  shipRoof2.rotation.z = Math.PI / 2;
+  shipRoof2.castShadow = true;
+
+  
+  shipGroup.add(shipBody);
+  shipGroup.add(shipTip);
+  shipGroup.add(shipRoof1);
+  shipGroup.add(shipRoof2);
+  shipGroup.position.set(0,10,600);
+  shipGroup.scale.set(20,20,20);
+  scene.add(shipGroup);
+/*
+  var start = { x: shipGroup.position.x, y: shipGroup.position.y, z: shipGroup.position.z };
+  var target1 = { x: shipGroup.position.x, y: shipGroup.position.y-0.5, z: shipGroup.position.z};
+  var target2 = { x: shipGroup.position.x, y: shipGroup.position.y, z: shipGroup.position.z };
+  var tween1 = new TWEEN.Tween(start).to(target1, 5000).easing(TWEEN.Easing.Elastic.InOut)
+  var tween2 = new TWEEN.Tween(start).to(target2, 5000).easing(TWEEN.Easing.Elastic.InOut).chain(tween1)
+  tween1.chain(tween2)
+  tween1.start()
+  const update = function () {
+    shipGroup.position.x = start.x;
+    shipGroup.position.y = start.y;
+    shipGroup.position.z = start.z;
+  }
+  tween1.onUpdate(update)
+  tween2.onUpdate(update);
+*/
+}
+
+// box
+function buildBox() {
+  const geometry = new THREE.BoxGeometry(3, 10, 3);
+  // Repeat the texture to fill everything, and keep the same sizes in this case.
+  const texture = new THREE.TextureLoader().load('assets/Body.jpg');
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set( 1, 1 );
+  // Add a bump map to be a little more realistic, not required.
+  const material = new THREE.MeshPhongMaterial( { color: 0xFF0000 } );
+  let box = new THREE.Mesh(geometry, material);
+  box.position.set(490, 0, 490)
+  box.castShadow = true;
+  box.receiveShadow = true;
+  box.material.transparent = true;
+  scene.add(box)
+
+  var start = { x: 90, y: 0, z: 490 };
+  var target1 = { x: 50, y: 5, z: 490};
+  var target2 = { x: 80, y: 5, z: 490 };
+  var tween1 = new TWEEN.Tween(start).to(target1, 10000).easing(TWEEN.Easing.Elastic.InOut)
+  var tween2 = new TWEEN.Tween(start).to(target2, 10000).easing(TWEEN.Easing.Elastic.InOut).chain(tween1)
+  tween1.chain(tween2)
+  tween1.start()
+  const update = function () {
+    box.position.x = start.x;
+    box.position.y = start.y;
+    box.position.z = start.z;
+  }
+  tween1.onUpdate(update)
+  tween2.onUpdate(update);
+  
 }
 
 /*
@@ -252,10 +393,10 @@ function buildGround() {
 // and stays at ground level.
 function buildTrees() {
   // Generate a random number to determine the amount of trees.
-  const treesToAdd = Math.floor(Math.random() * 50) + 10;
+  const treesToAdd = 100;
   for( let i = 0; i <= treesToAdd; i++) {
-    let randomX = -1 * (Math.floor(Math.random() * 500)-500)* randomSign();
-    let randomZ = (Math.floor(Math.random() * 500)-500) * randomSign();
+    let randomX = -1 * (Math.floor(Math.random() * 500)-400)* randomSign();
+    let randomZ = (Math.floor(Math.random() * 500)-400) * randomSign();
     let newTree = buildTree(randomX, randomZ);
     scene.add(newTree);
   }
@@ -348,14 +489,12 @@ function buildClouds() {
  // Function to build a cloud.
  // Using a box geometry as its base.
 function buildCloud(x, z) {
-  let randomX = Math.floor(Math.random() * 50) + 20;
-  let randomY = Math.floor(Math.random() * 11) + 10;
-  let randomZ = Math.floor(Math.random() * 100) + 80;
+  let randomY = Math.floor(Math.random() * 50) + 500;
   const geometry = new THREE.BoxGeometry(0, 0, 0);
   //const geometry = new THREE.BoxGeometry(50, 15, 100);
   const material = new THREE.MeshPhongMaterial( { color: 0xFFFFFF } );
   let clouds = new THREE.Mesh(geometry, material);
-  clouds.position.set(x, 500, z);
+  clouds.position.set(x, randomY, z);
   let particles = buildCloudParticles();
   clouds.add(particles);
   return clouds;
@@ -364,9 +503,10 @@ function buildCloud(x, z) {
 // Function to build a tree's leaves.
 // Using a box geometry as the base on top of the tree log.
 function buildCloudParticles() {
+  let randomX = Math.floor(Math.random() * 100)+ 100;
+  let randomZ = Math.floor(Math.random() * 100)+ 500;
   const geometry = new THREE.BoxGeometry(100, 10, 100);
   const material = new THREE.MeshPhongMaterial( { color: 0xFFFFFF } );  let leaves = new THREE.Mesh(geometry, material);
-  leaves.position.y = 10;
   const leavesToAdd = Math.floor(Math.random() * 8) + 2;
   for( let i = 0; i <= leavesToAdd; i++) {
     leaves.add(buildCloudsRandom());
@@ -378,12 +518,13 @@ function buildCloudParticles() {
 // Using a box geometry that's between some ranges depending on the component.
 // This way each tree is unique in a way.
 function buildCloudsRandom() {
-  let randomX = Math.floor(Math.random() * 300) + 150;
-  let randomZ = Math.floor(Math.random() * 400) + 200;
-  const geometry = new THREE.BoxGeometry(randomX / 30, 10, randomZ / 30);
+  let randomX = Math.floor(Math.random() * 50) + 10;
+  let randomY = (Math.floor(Math.random() * 5) + 10) * randomSign();
+  let randomZ = Math.floor(Math.random() * 50) + 10;
+  const geometry = new THREE.BoxGeometry(randomX, randomY, randomZ);
   const material = new THREE.MeshPhongMaterial( { color: 0xFFFFFF } );
   let leaves = new THREE.Mesh(geometry, material);
-  leaves.position.y = Math.floor(Math.random() * 15);
+  leaves.position.y = Math.floor(Math.random() * 5) + 10;
   leaves.position.x = Math.floor(Math.random() * 260 / 30) * randomSign();
   leaves.position.z = Math.floor(Math.random() * 180 / 30) * randomSign();
   return leaves;
@@ -407,6 +548,8 @@ function onWindowResize() {
   renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
+  init();
+  animate();
 
 
 
